ecbea10bd3e14742844586c1bcacfa0b5038835c
amntllhz <amanatullah1904@gmail.com>
2025-06-14 00:23:40 +0700

    style: root style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,10 +10,8 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <section class="max-w-5xl w-full mx-auto flex flex-col justify-center items-center min-h-screen">
-        <div class="relative flex max-w-md justify-center gap-4 items-center bg-apple-50 rounded-2xl mx-auto ring ring-apple-600">
-           <h1 class="p-10 font-display font-bold">Halaman Home</h1>
-        </div>
+    <section class="max-w-5xl w-full mx-auto flex flex-col justify-center items-center min-h-screen bglin">        
+            
     </section>
 </body>
 </html>
