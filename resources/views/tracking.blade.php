e7f4ad8bd58981adbf8b0cb7558adfe6e95887d0
amntllhz <amanatullah1904@gmail.com>
2025-06-14 22:42:12 +0700

    sect: tracking sect base

--- resources/views/tracking.blade.php
+++ resources/views/tracking.blade.php
@@ -9,22 +9,25 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <section class="max-w-5xl w-full mx-auto flex flex-col">
-        <div>
-            <h1 class="text-3xl text-blue-400 font-bold">Status Pengajuan </h1>
-            <p class="text-gray-500 text-sm">Cek status pengajuan secara realtime</p>        
+    <section class="lg:max-w-4xl xs:max-w-9/10 w-full mx-auto flex flex-col justify-center items-center mt-6">
+        <div class="flex flex-col justify-center items-center gap-1">
+            <h1 class="text-apple-600 lg:text-3xl xs:text-2xl font-bold font-display text-center w-full">Cek Status Pengajuan</h1>
+            <p class="text-gray-400 text-sm font-display">Lakukan pengecekan status pengajuan secara berkala</p>           
         </div>
 
         @if(session('error'))
             <p style="color: red;">{{ session('error') }}</p>
         @endif    
 
-        <form action="{{ route('tracking.result') }}" method="POST">
+        <form action="{{ route('tracking.result') }}" method="POST" class="lg:max-w-3xl xs:max-w-9/10 flex flex-col gap-4 justify-center w-full ring ring-apple-600 p-4 rounded-2xl mt-6">
             @csrf
-            <label for="id" class="block mb-2 text-sm font-medium text-gray-400">Id Pengajuan</label>            
-            <input name="id" id="id" type="text" class=" bg-gray-50 border border-gray-300 text-gratwo text-xs rounded-lg placeholder:text-gray-400 placeholder:text-xs focus:ring-green-400 focus:border-green-400 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-300 dark:text-white dark:focus:ring-green-400 dark:focus:border-green-400" placeholder="Tuliskan nama" required autocomplete="off"/>
-            <button type="submit">Cek status</button>
+            <div class="flex flex-col gap-2">
+                <label for="id" class="block text-xs font-display text-apple-600 font-semibold">ID Pengajuan</label>            
+                <input name="id" id="id" type="text" class=" bg-gray-50 border border-apple-600 font-display text-xs rounded-lg placeholder:text-gray-400 placeholder:text-xs focus:ring-apple-600 focus:border-green-400 block w-full p-2.5" placeholder="Tuliskan ID Pengajuan" required autocomplete="off"/>
+            </div>
+            <button type="submit" class="text-white text-center font-display font-bold bg-gradient-to-b from-apple-600 to-apple-700 hover:from-apple-700 hover:to-apple-900 transition duration-300 ease-in-out px-4 py-2 text-sm rounded-xl w-full cursor-pointer">Cek status</button>
         </form>
+
     </section>
 </body>
 </html>
