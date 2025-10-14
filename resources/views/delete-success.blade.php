f76828bc2f55e7b96786606f633f0ba0044b7afc
amntllhz <amanatullah1904@gmail.com>
2025-10-14 12:51:20 +0700

    feat: delete success page

--- resources/views/delete-success.blade.php
+++ resources/views/delete-success.blade.php
@@ -13,23 +13,21 @@
     <x-navbar></x-navbar>
     <section class="lg:max-w-5xl xs:max-w-9/10 w-full mx-auto flex flex-col justify-center items-center min-h-screen">
         <div class="items-center justify-center min-h-screen w-full flex">
-            <div class="flex flex-col gap-2 bg-white p-4 lg:max-w-xs xs:max-w-9/10 w-full rounded-xl ring ring-apple-600 text-center">
+            <div class="flex flex-col gap-2 bg-gradient-to-b from-white to-apple-200/50 p-4 lg:max-w-xs xs:max-w-9/10 w-full rounded-xl text-center">
 
-                <div class="bg-apple-900/10 p-2.5 rounded-full w-fit mx-auto mt-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 text-apple-600">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
-                    </svg>                        
-                </div>
-
-                <div class="flex flex-col mt-2">
-                    <h2 class="font-display font-bold text-base text-apple-600 mb-1">Pengajuan Berhasil Dihapus </h2>
-                    <p class="font-display text-xs max-w-xs text-gray-400 mb-3">
-                        Pengajuan anda berhasil dihapus
-                    </p>
+                <div class="flex flex-col gap-4 justify-center w-full">
+                    <div class="bg-apple-white justify-center items-center flex flex-col rounded-2xl p-6">
+                        <img class="h-32" src="{{ asset('img/delsuccess/delete-success.svg') }}" alt="">
+                    </div>
+                    <div class="flex flex-col gap-1 lg:max-w-xs xs:w-full  mx-auto">
+                        <h1 class="text-apple-600 text-center font-semibold font-display">Pengajuan Berhasil Dihapus</h1>
+                        <p class="text-apple-600 text-center font-display text-xs">Lakukan pengajuan ulang di halaman pengajuan</p>
+                    </div>
                     <a href="{{ url('/home') }}" class="text-white font-display font-bold bg-gradient-to-b from-apple-600 to-apple-700 hover:from-apple-700 hover:to-apple-900 transition duration-300 ease-in-out px-4 py-2 text-sm rounded-md">
                         Kembali ke Beranda
                     </a>
                 </div>
+
             </div>
         </div>
     </section>
