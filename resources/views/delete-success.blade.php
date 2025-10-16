ed65583238c8363b50fbb4b2208e2a569639ddd2
amntllhz <amanatullah1904@gmail.com>
2025-10-16 19:46:05 +0700

    style: restyle delete success page

--- resources/views/delete-success.blade.php
+++ resources/views/delete-success.blade.php
@@ -13,19 +13,21 @@
     <x-navbar></x-navbar>
     <section class="lg:max-w-5xl xs:max-w-9/10 w-full mx-auto flex flex-col justify-center items-center min-h-screen">
         <div class="items-center justify-center min-h-screen w-full flex">
-            <div class="flex flex-col gap-2 bg-gradient-to-b from-white to-apple-200/50 p-4 lg:max-w-xs xs:max-w-9/10 w-full rounded-xl text-center">
+            <div class="flex flex-col gap-2 mt-10 bg-white p-4 lg:max-w-sm xs:max-w-9/10 w-full rounded-xl text-center">
 
                 <div class="flex flex-col gap-4 justify-center w-full">
-                    <div class="bg-apple-white justify-center items-center flex flex-col rounded-2xl p-6">
-                        <img class="h-32" src="{{ asset('img/delsuccess/delete-success.svg') }}" alt="">
+                    <div class="bg-gradient-to-b from-white to-apple-300/50 justify-center items-center flex flex-col rounded-lg p-6">
+                        <img class="h-40" src="{{ asset('img/delsuccess/delete-success.svg') }}" alt="">
                     </div>
-                    <div class="flex flex-col gap-1 lg:max-w-xs xs:w-full  mx-auto">
-                        <h1 class="text-apple-600 text-center font-semibold font-display">Pengajuan Berhasil Dihapus</h1>
-                        <p class="text-apple-600 text-center font-display text-xs">Lakukan pengajuan ulang di halaman pengajuan</p>
+                    <div class="flex flex-col space-y-4 justify-center items-center">
+                        <div class="flex flex-col gap-1 lg:max-w-xs xs:w-full  mx-auto">
+                            <h1 class="text-gray-600 text-center font-bold font-display">Pengajuan Berhasil Dihapus</h1>
+                            <p class="text-gray-400 text-center font-display text-xs">Lakukan pengajuan ulang di halaman pengajuan, pastikan data yang anda masukan sudah benar dan sesuai dengan ketentuan yang berlaku</p>
+                        </div>
+                        <a href="{{ url('/home') }}" class="text-white font-display w-fit font-bold bg-gradient-to-b from-apple-600 to-apple-700 hover:from-apple-700 hover:to-apple-900 transition duration-300 ease-in-out px-4 py-2 text-sm rounded-lg">
+                            Kembali ke Beranda
+                        </a>
                     </div>
-                    <a href="{{ url('/home') }}" class="text-white font-display font-bold bg-gradient-to-b from-apple-600 to-apple-700 hover:from-apple-700 hover:to-apple-900 transition duration-300 ease-in-out px-4 py-2 text-sm rounded-md">
-                        Kembali ke Beranda
-                    </a>
                 </div>
 
             </div>
