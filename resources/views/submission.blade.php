8388ec5c21328d8d247a2db419e383eb6e1cd2ff
amntllhz <amanatullah1904@gmail.com>
2025-11-08 21:56:57 +0700

    style: update option style

--- resources/views/submission.blade.php
+++ resources/views/submission.blade.php
@@ -364,7 +364,7 @@
                                     >
                                         <polyline points="20 6 9 17 4 12"></polyline>
                                     </svg>
-                                    <span class="font-display" x-text="item.title"></span>
+                                    <span class="font-display font-medium text-xs" x-text="item.title"></span>
                                 </li>
                             </template>
                         </ul>
@@ -421,7 +421,7 @@
                                     <svg x-show="selectedItem && selectedItem.value === item.value" class="absolute left-2 w-3.5 h-3.5 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                         <polyline points="20 6 9 17 4 12"></polyline>
                                     </svg>
-                                    <span class="font-display" x-text="item.title"></span>
+                                    <span class="font-display font-medium text-xs" x-text="item.title"></span>
                                 </li>
                             </template>
                         </ul>
@@ -478,7 +478,7 @@
                                     <svg x-show="selectedItem && selectedItem.value === item.value" class="absolute left-2 w-3.5 h-3.5 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                         <polyline points="20 6 9 17 4 12"></polyline>
                                     </svg>
-                                    <span class="font-display" x-text="item.title"></span>
+                                    <span class="font-display font-medium text-xs" x-text="item.title"></span>
                                 </li>
                             </template>
                         </ul>
@@ -535,7 +535,7 @@
                                     <svg x-show="selectedItem && selectedItem.value === item.value" class="absolute left-2 w-3.5 h-3.5 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                         <polyline points="20 6 9 17 4 12"></polyline>
                                     </svg>
-                                    <span class="font-display" x-text="item.title"></span>
+                                    <span class="font-display font-medium text-xs" x-text="item.title"></span>
                                 </li>
                             </template>
                         </ul>
