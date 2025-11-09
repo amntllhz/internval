10c4e6867dcdd44d802ee40f7717754e606cee58
amntllhz <amanatullah1904@gmail.com>
2025-11-09 13:45:52 +0700

    style: update style error page

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,7 @@
 @extends('errors::minimal')
 
 @section('title', __('Forbidden'))
-@section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('code', '403 - Forbidden')
+@section('message', __($exception->getMessage() ?: '
+    	Server memahami permintaan Anda sepenuhnya, tetapi menolak akses. Anda tidak memiliki izin atau hak istimewa yang diperlukan untuk melihat konten ini
+'))
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,7 @@
 @extends('errors::minimal')
 
 @section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@section('code', '404 - Not Found')
+@section('message', __('
+    Maaf, halaman web atau sumber daya spesifik yang Anda cari tidak dapat ditemukan di server kami saat ini. Kami mohon maaf atas ketidaknyamanan ini
+'))
