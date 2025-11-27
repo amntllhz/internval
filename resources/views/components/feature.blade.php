5e0baffd22b2c6bffaf414a6acb1411c71d8312a
amntllhz <amanatullah1904@gmail.com>
2025-11-27 23:44:50 +0700

    style: update feature section

--- resources/views/components/feature.blade.php
+++ resources/views/components/feature.blade.php
@@ -9,35 +9,37 @@
     {{-- Fitur --}}
     <div class="flex md:flex-row xs:flex-col gap-6 md:w-3/4 lg:w-2/3 xs:w-full justify-center rounded-xl">
         <div class="grid lg:grid-cols-2 md:grid-cols-2 xs:grid-cols-1 gap-6 justify-center items-center">
-            <div class="flex flex-col gap-4 justify-center w-full ring ring-apple-600 p-4 rounded-3xl">
-                <div class="bg-apple-900/5 justify-center items-center flex flex-col rounded-xl p-6">
+
+            {{-- Kirim Pengajuan --}}
+            <div class="flex flex-col gap-3 justify-center w-full ring ring-apple-600 p-3 rounded-3xl">
+                <div class="bg-gradient-to-b from-white to-apple-200/50 justify-center items-center flex flex-col rounded-xl p-6">
                     <img class="h-32" src="{{ asset('img/feature/sendsub2.svg') }}" alt="">
                 </div>
                 <div class="flex flex-col lg:gap-2 xs:gap-1 w-full mx-auto">
                     <a href={{ url('/submission') }} class="flex justify-center items-center text-white text-center font-display font-bold bg-gradient-to-b from-apple-600 to-apple-700 hover:from-apple-700 hover:to-apple-900 transition duration-300 ease-in-out px-4 py-2 text-sm rounded-xl w-full">
                         Kirim Pengajuan                                                                           
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 ml-2">
-                            <path fill-rule="evenodd" d="M5.625 1.5H9a3.75 3.75 0 0 1 3.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 0 1 3.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 0 1-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875Zm6.905 9.97a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72V18a.75.75 0 0 0 1.5 0v-4.19l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z" clip-rule="evenodd" />
-                            <path d="M14.25 5.25a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963A5.23 5.23 0 0 0 16.5 7.5h-1.875a.375.375 0 0 1-.375-.375V5.25Z" />
-                        </svg>                        
+                            <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
+                        </svg>
                     </a>                    
                 </div>
             </div>
-            <div class="flex flex-col gap-4 justify-center w-full ring ring-apple-600 p-4 rounded-3xl">
-                <div class="bg-apple-900/5 justify-center items-center flex flex-col rounded-xl p-6">
+
+            {{-- Cek Status Pengajuan --}}
+            <div class="flex flex-col gap-3 justify-center w-full ring ring-apple-600 p-3 rounded-3xl">
+                <div class="bg-gradient-to-b from-white to-apple-200/50 justify-center items-center flex flex-col rounded-xl p-6">
                     <img class="h-32" src="{{ asset('img/feature/tracking.svg') }}" alt="">
                 </div>
                 <div class="flex flex-col lg:gap-2 xs:gap-1 w-full mx-auto">
                     <a href={{ url('/tracking') }} class="flex justify-center items-center text-white text-center font-display font-bold bg-gradient-to-b from-apple-600 to-apple-700 hover:from-apple-700 hover:to-apple-900 transition duration-300 ease-in-out px-4 py-2 text-sm rounded-xl w-full">
                         Cek Status Pengajuan
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 ml-2">
-                            <path fill-rule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z" clip-rule="evenodd" />
-                            <path fill-rule="evenodd" d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375ZM6 12a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V12Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 15a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V15Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 18a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V18Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+                            <path fill-rule="evenodd" d="M8.161 2.58a1.875 1.875 0 0 1 1.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0 1 21.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 0 1-1.676 0l-4.994-2.497a.375.375 0 0 0-.336 0l-3.868 1.935A1.875 1.875 0 0 1 2.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437ZM9 6a.75.75 0 0 1 .75.75V15a.75.75 0 0 1-1.5 0V6.75A.75.75 0 0 1 9 6Zm6.75 3a.75.75 0 0 0-1.5 0v8.25a.75.75 0 0 0 1.5 0V9Z" clip-rule="evenodd" />
                         </svg>
-
                     </a>                    
                 </div>
-            </div>                       
+            </div> 
+
         </div>
     </div>
     
