8d5d1b9a7e265fbbbc1ad0801cb1f7ea8b78409e
amntllhz <amanatullah1904@gmail.com>
2025-12-14 13:16:31 +0700

    dep + style: syncronize role option on start

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -37,7 +37,7 @@
                             <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                             <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
                         </svg>
-                        Dosen
+                        Kaprodi
                     </a>
                     <a href="/admin/login" class="flex justify-center items-center gap-2 lg:text-gray-500 xs:text-white font-display font-bold lg:bg-gray-200 xs:bg-transparent lg:hover:bg-gray-300 xs:hover:bg-white/30 lg:hover:text-white xs:ring xs:ring-white transition duration-300 ease-in-out px-4 py-2 lg:text-xs xs:text-sm rounded-md lg:w-[108px] xs:w-full text-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-workspace" viewBox="0 0 16 16">
