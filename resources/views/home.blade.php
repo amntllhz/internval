0847bfa44b35fc66542858db988e15b989103ca7
amntllhz <amanatullah1904@gmail.com>
2025-12-19 19:11:39 +0700

    dep + style: fix spacing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <x-navbar></x-navbar>
     <x-hero></x-hero>
-    <div class="flex flex-col justify-center items-center lg:gap-20 md:gap-16 xs:gap-12 lg:mt-0 md:-mt-[40rem] xs:-mt-[30rem]">
+    <div class="flex flex-col justify-center items-center lg:gap-20 md:gap-16 xs:gap-12 lg:mt-0 md:-mt-[40rem] xs:-mt-[24rem]">
         <x-flow></x-flow>
         <x-feature></x-feature>
         <x-footer></x-footer>
