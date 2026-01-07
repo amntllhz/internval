89b9af7e3d3f5a9ab65a3dde94e607190e22ac4a
amntllhz <amanatullah1904@gmail.com>
2026-01-07 14:17:44 +0700

    dep + feat: undo try disable dark mode

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -3,9 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="color-scheme" content="light">
-    <meta name="theme-color" content="#ffffff">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">    
     <title>Internval - @yield('title')</title>
     <link rel="icon" href={{ asset('img/icon.svg') }}>
     <link href="https://api.fontshare.com/v2/css?f[]=clash-display@200,300,400,500,600,700&display=swap" rel="stylesheet">
